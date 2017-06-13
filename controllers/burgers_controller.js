3107cc5a2d735d3be6d05c79071bd07ecfef6105
Damien Asseya <damienasseya@Users-MacBook-Pro.local>
2017-06-13 13:32:16 -0400

    Fixed connection to db

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,30 +1,31 @@
-// This file sets up all of the routes for the application. MySQL queries are in here.
 var express = require('express');
 var router = express.Router();
-var burger = require('../models/burger.js');
-var bodyParser = require('body-parser');
-var connection = require('../config/connection.js');
+var db = require('../models');
 
-//Grabs the data from the burgers table...all of the burgers! These are sorted later with Handlebars #if and #unless statements
+//Grabs all the data from the burgers table...all of the burgers! These are sorted later with Handlebars #if and #unless statements
 router.get("/", function(req, res) {
-  burger.selectAll(function(data) {
 
-    res.render("index", {
-      burgers: data
-    });
-  });
+  db.Burger.findAll({}).then(function(data) {
+    res.render("index", {burgers: data});
+  })
 });
 
-//User can enter a new burger into the system. Default value of devoured=false, so user doesn't actually enter that
+//User can enter a new burger into the system. Default values are already set for id, devoured=false, createdAt and updatedAt
 router.post("/", function(req, res) {
-  burger.insertOne(req.body.burger_name, function() {
+  db.Burger.create({burger_name: req.body.burger_name}).then(function() {
     res.redirect("/");
   });
 });
 
 //Changes status of burger from devoured=false to devoured=true
 router.put("/:id", function(req, res) {
-  burger.updateOne(req.params.id, function() {
+  db.Burger.update({
+    devoured: true
+  }, {
+    where: {
+      id: req.params.id
+    }
+  }).then(function() {
     res.redirect("/");
   });
 });
--- models/burger.js
+++ models/burger.js
@@ -1,23 +1,23 @@
-var orm = require("../config/orm.js");
-var express = require("express");
-var app = express();
+var Sequelize = require('sequelize');
 
-var burger = {
-  selectAll: function(cb) {
-    orm.selectAll('burgers', function(res) {
-      cb(res);
-    });
-  },
-  insertOne: function(value, cb) {
-    orm.insertOne('burgers', "burger_name", value, function(res) {
-      cb(res);
-    });
-  },
-  updateOne: function(condition, cb) {
-    orm.updateOne('burgers', 'devoured', '1', 'id', condition, function(res) {
-      cb(res);
-    })
-  }
+module.exports = function(sequelize, DataTypes) {
+
+  var Burger = sequelize.define("Burger", {
+
+    burger_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    devoured: {
+      type: Sequelize.BOOLEAN,
+      allowNull: false,
+      defaultValue: false
+    }
+  });
+  Burger.sync();
+  return Burger;
 
 }
-module.exports = burger;
